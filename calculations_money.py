e4daed9d3a1dbb37cf2ec1c38247a7aea2a6ff01
DimastX <mulishovd|@icloud.com>
2023-07-27 13:11:59 +0300

    отредактировал отмывку

--- calculations_money.py
+++ calculations_money.py
@@ -3,6 +3,10 @@ import math
 def clear_calculations(session, df):
     by_x = math.floor(df['Значение'][3] / float(session['second_form']['width']))
     by_y = math.floor(df['Значение'][2] / float(session['second_form']['length']))
-    number_multi = by_x * by_y
+    number_multi = int(by_x * by_y)
     number_items = int(session['second_form']['length_num']) * int(session['second_form']['width_num'])
-    return [number_multi, number_items]
+    number_smallitems = int(number_items * number_multi)
+    if int(session['home_form']['field3']) < number_smallitems:
+        number_smallitems = int(session['home_form']['field3'])
+        number_multi = number_smallitems / number_items
+    return [number_multi, number_items, number_smallitems]
