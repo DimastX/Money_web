ad75a2c5922b0355ed45748d17eda03bae04a87a
DimastX <mulishovd|@icloud.com>
2023-08-05 22:35:26 +0300

    Поправил старые idшники

--- tables.py
+++ tables.py
@@ -1,31 +1,32 @@
 import pandas as pd
 def tables(file):
-    df = pd.read_csv(file, encoding='utf-8', sep=';')
+    df = pd.read_csv(file, encoding='utf-8', sep=',')
     PAP = df.iloc[:, :2]
     BOM = df.iloc[:, 2:]
     Bot = PAP[PAP["Layer"] == 'Bot']["Designator"]
     Bot = pd.DataFrame(Bot)
     Top = PAP[PAP["Layer"] == 'Top']["Designator"]
     Top = pd.DataFrame(Top)
+    top_lines = 0
+    bot_lines = 0
     for index, row in Bot.iterrows():
         value = str(row['Designator'])
         for index2, row2, in BOM.iterrows():
-            value2 = str(row2['Designators (BOM)'])
+            value2 = str(row2['Designators (BOM)']).split(",")
             if value in value2:
                 Bot.at[index, 'Name'] = row2["Name"]
-    
+                bot_lines += 1
     for index, row in Top.iterrows():
         value = str(row['Designator'])
         for index2, row2, in BOM.iterrows():
-            value2 = str(row2['Designators (BOM)'])
+            value2 = str(row2['Designators (BOM)']).split(",")
             if value in value2:
                 Top.at[index, 'Name'] = row2["Name"]
-    top_lines = Top.shape[0]
+                top_lines += 1
     if top_lines:
         top_lines_unic = Top["Name"].nunique()
     else:
         top_lines_unic = 0
-    bot_lines = Bot.shape[0]
     if bot_lines:
         bot_lines_unic = Bot["Name"].nunique()
     else:
