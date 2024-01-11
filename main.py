33187a35510c08e8f557af2b230efecf14fdffdb
DimastX <mulishovd@icloud.com>
2024-01-11 09:21:12 +0300

    logs

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ import time
 
 from datetime import datetime
 import os
+
+import logging
+
+# Настройка логирования
+logging.basicConfig(filename='mylog.log', level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 #Проверка
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -118,7 +124,7 @@ def list_directories():
                     })
                     
 
-    return render_template('Dirs2.html', file_data=file_data, dir_exists=dir_exists, selected_path=selected_path, os=os,
+    return render_template('Dirs4.html', file_data=file_data, dir_exists=dir_exists, selected_path=selected_path, os=os,
                            parent_directory=parent_directory)
 
 @app.route('/copy', methods=['POST'])
